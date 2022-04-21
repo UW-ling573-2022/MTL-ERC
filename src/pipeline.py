8d4b73017fb9d5327262f086f1f34563691e113b
Junyin Chen <VulpesVulpes825@gmail.com>
2022-04-21 21:20:42 +0800

    feat: Use TextDataset in pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,26 +1,39 @@
 import torch
+from load_data import TextDataset
 from data import preprocess
 from model import ERCModel
 from train import train
 
+
 def pipeline():
-    data_files = {"train": "data/train_sent_emo.csv", 
-                  "validation": "data/dev_sent_emo.csv", 
-                  "test": "data/test_sent_emo.csv"}
     checkpoint = "roberta-base"
-    train_dataloader, eval_dataloader, test_dataloader = preprocess(checkpoint, data_files)
+    ds_train = TextDataset(
+        dataset="MELD",
+        split="train",
+        speaker_mode=True,
+        num_past_utterances=1,
+        num_future_utterances=1,
+    )
+    ds_eval = TextDataset(
+        dataset="MELD",
+        split="dev",
+        speaker_mode=True,
+        num_past_utterances=1,
+        num_future_utterances=1,
+    )
     model = ERCModel(checkpoint, 7, 7)
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-5, epochs=5, steps_per_epoch=len(train_dataloader))
-    train(model, 
-          train_dataloader, 
-          eval_dataloader,
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-5, epochs=5, steps_per_epoch=len(ds_train))
+    train(model,
+          ds_train,
+          ds_eval,
           epochs=5,
           loss_fn=loss_fn,
           optimizer=optimizer,
           scheduler=scheduler,
-          device="cuda")
+          )
+
 
 if __name__ == "__main__":
    pipeline()
