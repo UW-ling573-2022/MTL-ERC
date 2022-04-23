807ebefae50f2524e2db215036475fcf76685d02
Junyin Chen <VulpesVulpes825@gmail.com>
2022-04-23 21:10:50 +0800

    chore: Add addtional field in util.py

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,4 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ~/anaconda3/envs/UW-LING-573
 
 # put your command for running word2vec.py here
-python src/pipeline.py --do_train
+python src/pipeline.py
--- src/util.py
+++ src/util.py
@@ -56,7 +56,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "--model_file",
         type=str,
-        default=None,
+        default=get_root_dir() + "outputs/D2",
         help="Path to stored model",
     )
     parser.add_argument(
