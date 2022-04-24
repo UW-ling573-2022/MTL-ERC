1ed9fcc2fdadb899802ad549d42c3cc93c3e31ad
Junyin Chen <VulpesVulpes825@gmail.com>
2022-04-24 13:39:32 +0800

    fix: Add too many future utterance in preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -30,7 +30,7 @@ def preprocess(tokenizer, labels, **kwargs):
                 past_speaker = labels["Speaker"].int2str(past["Speaker"])
                 past_utterance = past["Utterance"]
                 example["Past"] += past_speaker + ":" + past_utterance
-                if past["Utterance_ID"] == 0 or i == kwargs["num_past_utterances"]:
+                if past["Utterance_ID"] == 0 or i >= kwargs["num_past_utterances"]:
                     break
                 i += 1
 
@@ -41,9 +41,9 @@ def preprocess(tokenizer, labels, **kwargs):
                 future_speaker = labels["Speaker"].int2str(future["Speaker"])
                 future_utterance = future["Utterance"]
                 example["Future"] += future_speaker + ":" + future_utterance
-                i += 1
-                if future["Utterance_ID"] == 0 or i == kwargs["num_future_utterances"] - 1:
+                if future["Utterance_ID"] == 0 or i >= kwargs["num_future_utterances"] - 1:
                     break
+                i += 1
 
         return example
 
