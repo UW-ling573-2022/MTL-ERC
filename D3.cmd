3f064c0ffaa294fa80b737afbf92ea010cc7e676
Junyin Chen <VulpesVulpes825@gmail.com>
2022-05-30 11:39:56 +0800

    fix: Change condor log output

--- D3.cmd
+++ D3.cmd
@@ -1,7 +1,7 @@
 executable = src/run.sh
-log=log/D2.log
-output=log/D2.out
-error=log/D2.error
+log=log/D3.log
+output=log/D3.out
+error=log/D3.error
 request_memory = 4096
 notification=Always
 request_gpus=1
--- D4_full.cmd
+++ D4_full.cmd
@@ -1,7 +1,7 @@
 executable = src/run_full.sh
-log=log/D2.log
-output=log/D2.out
-error=log/D2.error
+log=log/D4.log
+output=log/D4.out
+error=log/D4.error
 request_memory = 4096
 notification=Always
 request_gpus=1
